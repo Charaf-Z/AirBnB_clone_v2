eb7c9cf9dd3300a77e68fbe796fdd2210ee3e57f
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-17 00:45:41 +0100

    add skipIf statement to the test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -130,6 +130,7 @@ class TestHBNBCommand(unittest.TestCase):
             cursor.close()
             dbc.close()
 
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "DBStorage test")
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
@@ -138,6 +139,7 @@ class TestHBNBCommand(unittest.TestCase):
             result.total_errors, 0, "Found code style errors (and warnings)."
         )
 
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "DBStorage test")
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
