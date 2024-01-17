6bbe17cff9c7248b764b8344e2ad6f78983744a4
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-17 18:49:51 +0100

    new version of tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""Defines the unittests for the State model."""
+"""
+Defines the unittests for models/test_state.py
+"""
 import os
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBasemodel
 from models.state import State
 
 
-class test_state(test_basemodel):
-    """A unittest for State class."""
+class TestState(TestBasemodel):
+    """
+    A unittest for State class
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize the test class for State."""
+        """
+        Initializes the test class for State
+        """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """Tests the type of name attribute."""
+        """
+        Tests the type of name attribute
+        """
         new = self.value()
         self.assertEqual(
             type(new.name),
