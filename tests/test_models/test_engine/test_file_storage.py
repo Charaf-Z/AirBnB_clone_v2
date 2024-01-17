39bbe4ffd86588b4c0cec28f33eefd8456079ab7
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-17 21:07:06 +0100

    update the full test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,152 +1,89 @@
 #!/usr/bin/python3
-"""Module for testing file storage."""
-import os
+"""test for file storage"""
 import unittest
-from models import storage
-from models.base_model import BaseModel
+import pep8
+import os
+from models.user import User
+from models.engine.file_storage import FileStorage
 
 
-@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test")
-class test_fileStorage(unittest.TestCase):
-    """Test cases for FileStorage class."""
+class TestFileStorage(unittest.TestCase):
+    '''this will test the FileStorage'''
 
-    def setUp(self):
-        """Remove storage file at start of tests."""
-        del_list = []
-        for key in storage._FileStorage__objects.keys():
-            del_list.append(key)
-        for key in del_list:
-            del storage._FileStorage__objects[key]
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = FileStorage()
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
 
     def tearDown(self):
-        """Remove storage file at end of tests."""
+        """teardown"""
         try:
             os.remove("file.json")
         except Exception:
             pass
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_obj_list_empty(self):
-        """Tests that __objects is empty at start of tests."""
-        self.assertEqual(len(storage.all()), 0)
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_new(self):
-        """Tests that new objects are properly added to __objects."""
-        temp = BaseModel()
-        for obj in storage.all().values():
-            temp = obj
-            self.assertTrue(temp is obj)
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
     def test_all(self):
-        """Tests that all() returns __objects."""
-        new = BaseModel()
-        temp = storage.all()
-        self.assertIsInstance(temp, dict)
+        """tests if all works in File Storage"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_base_model_instantiation(self):
-        """Tests that a new instance of BaseModel is created."""
-        new = BaseModel()
-        self.assertFalse(os.path.exists("file.json"))
+    def test_new(self):
+        """test when new is created"""
+        storage = FileStorage()
+        obj = storage.all()
+        user = User()
+        user.id = 123455
+        user.name = "Kevin"
+        storage.new(user)
+        key = user.__class__.__name__ + "." + str(user.id)
+        self.assertIsNotNone(obj[key])
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_empty(self):
-        """Tests all() returns an empty dictionary if __objects is empty."""
-        new = BaseModel()
-        thing = new.to_dict()
-        new.save()
-        new2 = BaseModel(**thing)
-        self.assertNotEqual(os.path.getsize("file.json"), 0)
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_save(self):
-        """Tests that save() properly saves objects to file.json."""
-        new = BaseModel()
-        storage.save()
-        self.assertTrue(os.path.exists("file.json"))
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_reload(self):
-        """Tests that reload() properly loads objects from file.json."""
-        new = BaseModel()
-        storage.save()
-        storage.reload()
-        for obj in storage.all().values():
-            self.assertEqual(new.to_dict()["id"], obj.to_dict()["id"])
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_reload_empty(self):
-        """Tests that reload() properly loads an empty file.json."""
-        with open("file.json", "w") as f:
+    def test_reload_filestorage(self):
+        """
+        tests reload
+        """
+        self.storage.save()
+        Root = os.path.dirname(os.path.abspath("console.py"))
+        path = os.path.join(Root, "file.json")
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        try:
+            os.remove(path)
+        except:
             pass
-        with self.assertRaises(ValueError):
-            storage.reload()
+        self.storage.save()
+        with open(path, 'r') as f:
+            lines2 = f.readlines()
+        self.assertEqual(lines, lines2)
+        try:
+            os.remove(path)
+        except:
+            pass
+        with open(path, "w") as f:
+            f.write("{}")
+        with open(path, "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(self.storage.reload(), None)
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_reload_from_nonexistent(self):
-        """Tests that reload() properly loads from a nonexistent file.json."""
-        self.assertEqual(storage.reload(), None)
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_base_model_save(self):
-        """Tests that save() properly saves BaseModel objects to file.json."""
-        new = BaseModel()
-        new.save()
-        self.assertTrue(os.path.exists("file.json"))
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_type_path(self):
-        """Tests that __file_path is a string."""
-        self.assertEqual(type(storage._FileStorage__file_path), str)
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_type_objects(self):
-        """Tests that __objects is a dictionary."""
-        self.assertEqual(type(storage.all()), dict)
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_key_format(self):
-        """Tests that the key for each object in __objects."""
-        new = BaseModel()
-        new.save()
-        _id = new.to_dict()["id"]
-        for key in storage.all().keys():
-            temp = key
-        self.assertEqual(temp, "BaseModel" + "." + _id)
-
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage test"
-    )
-    def test_storage_var_created(self):
-        """Tests that a new instance of FileStorage is created."""
-        from models.engine.file_storage import FileStorage
-
-        self.assertEqual(type(storage), FileStorage)
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,51 +1,75 @@
 #!/usr/bin/python3
-"""Defines the unittests for the User model."""
-import os
-from tests.test_models.test_base_model import test_basemodel
-from models.user import User
+"""test for user"""
 import unittest
+import os
+from models.user import User
+from models.base_model import BaseModel
+import pep8
 
 
-class test_user(test_basemodel):
-    """A unittest for User class."""
+class TestUser(unittest.TestCase):
+    """this will test the User class"""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize the test class for User."""
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kevin"
+        cls.user.last_name = "Yook"
+        cls.user.email = "yook00627@gmamil.com"
+        cls.user.password = "secret"
 
-    def test_first_name(self):
-        """Tests the type of first_name attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.first_name),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
 
-    def test_last_name(self):
-        """Tests the type of last_name attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.last_name),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_email(self):
-        """Tests the type of email attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.email),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+    def test_pep8_User(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_password(self):
-        """Tests the type of password attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.password),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+    def test_checking_for_docstring_User(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(User.__doc__)
+
+    def test_attributes_User(self):
+        """chekcing if User have attributes"""
+        self.assertTrue('email' in self.user.__dict__)
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('password' in self.user.__dict__)
+        self.assertTrue('first_name' in self.user.__dict__)
+        self.assertTrue('last_name' in self.user.__dict__)
+
+    def test_is_subclass_User(self):
+        """test if User is subclass of Basemodel"""
+        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
+
+    def test_attribute_types_User(self):
+        """test attribute type for User"""
+        self.assertEqual(type(self.user.email), str)
+        self.assertEqual(type(self.user.password), str)
+        self.assertEqual(type(self.user.first_name), str)
+        self.assertEqual(type(self.user.first_name), str)
+
+    def test_save_User(self):
+        """test if the save works"""
+        self.user.save()
+        self.assertNotEqual(self.user.created_at, self.user.updated_at)
+
+    def test_to_dict_User(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.user), True)
 
 
 if __name__ == "__main__":
