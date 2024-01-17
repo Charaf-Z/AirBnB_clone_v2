d4173572a6a11f3cbd42ca35e002a6fbaea48ac7
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-17 21:25:43 +0100

    try another way for test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""test for BaseModel"""
+"""Test cases for BaseModel"""
 import unittest
 import os
 from models.base_model import BaseModel
-import pep8
 
 
 class TestBaseModel(unittest.TestCase):
@@ -28,13 +27,7 @@ class TestBaseModel(unittest.TestCase):
         except Exception:
             pass
 
-    def test_pep8_BaseModel(self):
-        """Testing for pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/base_model.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_checking_for_docstring_BaseModel(self):
+    def test_checking_for_docstring_basemodel(self):
         """checking for docstrings"""
         self.assertIsNotNone(BaseModel.__doc__)
         self.assertIsNotNone(BaseModel.__init__.__doc__)
@@ -42,22 +35,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
-    def test_method_BaseModel(self):
+    def test_method_basemodel(self):
         """chekcing if Basemodel have methods"""
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
 
-    def test_init_BaseModel(self):
+    def test_init_basemodel(self):
         """test if the base is an type BaseModel"""
         self.assertTrue(isinstance(self.base, BaseModel))
 
-    def test_save_BaesModel(self):
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == 'db', 'Not file engine')
+    def test_save_basemodel(self):
         """test if the save works"""
         self.base.save()
         self.assertNotEqual(self.base.created_at, self.base.updated_at)
 
-    def test_to_dict_BaseModel(self):
+    def test_to_dict_basemodel(self):
         """test if dictionary works"""
         base_dict = self.base.to_dict()
         self.assertEqual(self.base.__class__.__name__, 'BaseModel')
