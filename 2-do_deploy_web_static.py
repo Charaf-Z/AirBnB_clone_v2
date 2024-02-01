2dd4bb380e0a32eb59af901459477ab2a4992fb2
Charaf-Z <charafomarzitouni@gmail.com>
2024-02-01 18:42:04 +0100

    add pack to deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 """Deploy web_static content to remote servers."""
 
+from datetime import datetime
 from os.path import isfile, basename
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
 
 env.hosts = ["18.234.129.123", "52.3.244.13"]
 env.user = "ubuntu"
+
+
+
+def do_pack():
+    """
+    Create a compressed archive of the web_static folder.
+
+    Returns:
+        str: The file path of the created archive,
+            or None if the process fails.
+    """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+    if local("sudo mkdir -p versions").failed is True:
+        return None
+    if local("sudo tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return file_name
+
 
 
 def do_deploy(archive_path):
