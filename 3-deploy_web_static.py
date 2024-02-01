b0ee2a9ddc2576dc64d185b999fc2d2d4702b255
Charaf-Z <charafomarzitouni@gmail.com>
2024-02-01 20:19:58 +0100

    update 1.3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 from fabric.api import local
-from os.path import isfile
+from os.path import exists
 from os.path import basename
 from os.path import isdir
 from os.path import getsize
@@ -50,31 +50,24 @@ def do_deploy(archive_path):
     Raises:
         Exception: If an error occurs during the deployment process.
     """
-    if isfile(archive_path) is False:
+    if exists(archive_path) is False:
         return False
     file_name = basename(archive_path).split(".")[0]
-    file = f"/data/web_static/releases/{file_name}/"
-    tmp = f"/tmp/{file_name}.tgz"
-    if put(archive_path, "/tmp/").failed is True:
+    file = "/data/web_static/releases/{}/".format(file_name)
+    tmp = "/tmp/{}.tgz".format(file_name)
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(file))
+        run("tar -xzf {} -C {}".format(tmp, file))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(file, file))
+        run("rm -rf {}/web_static".format(file))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(file))
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-    if run("rm -rf {}".format(file)).failed is True:
-        return False
-    if run("mkdir -p {}".format(file)).failed is True:
-        return False
-    if run("tar -xzf {} -C {}".format(tmp, file)).failed is True:
-        return False
-    if run("rm {}".format(tmp)).failed is True:
-        return False
-    if run("mv {}/web_static/* {}/".format(file, file)).failed is True:
-        return False
-    if run("rm -rf {}/web_static".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s {} /data/web_static/current".format(file)).failed is True:
-        return False
-    print("New version deployed!")
-    return True
 
 
 def deploy():
