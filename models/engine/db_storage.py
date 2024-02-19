f3aaf01604d049e74403ba19631d734b4674b0c5
Charaf-Z <charafomarzitouni@gmail.com>
2024-02-19 03:26:01 +0100

    upload all the files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -45,7 +45,9 @@ class DBStorage:
                     obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
                     objects[obj_key] = obj
         else:
-            query = self.__session.query(cls)
+            query = self.__session.query(
+                cls if type(cls) is not str else eval(cls)
+            )
             for obj in query.all():
                 obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
                 objects[obj_key] = obj
