42ee5ac7da8b4a0d665f3e180ee1cb7233c82e51
Charaf-Z <charafomarzitouni@gmail.com>
2024-02-19 04:40:15 +0100

    state file update 1.3

--- models/place.py
+++ models/place.py
@@ -8,25 +8,24 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-if os.getenv("HBNB_TYPE_STORAGE", None) == "db":
-    place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column(
-            "place_id",
-            String(60),
-            ForeignKey("places.id"),
-            nullable=False,
-            primary_key=True,
-        ),
-        Column(
-            "amenity_id",
-            String(60),
-            ForeignKey("amenities.id"),
-            nullable=False,
-            primary_key=True,
-        ),
-    )
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        nullable=False,
+        primary_key=True,
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        nullable=False,
+        primary_key=True,
+    ),
+)
 
 """
 Represents the many to many relationship table
@@ -69,28 +68,72 @@ class Place(BaseModel, Base):
     """
 
     __tablename__ = "places"
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    city_id = (
+        Column(String(60), ForeignKey("cities.id"), nullable=False)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    user_id = (
+        Column(String(60), ForeignKey("users.id"), nullable=False)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    name = (
+        Column(String(128), nullable=False)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    description = (
+        Column(String(1024), nullable=True)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    number_rooms = (
+        Column(Integer, nullable=False, default=0)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0
+    )
+    number_bathrooms = (
+        Column(Integer, nullable=False, default=0)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0
+    )
+    max_guest = (
+        Column(Integer, nullable=False, default=0)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0
+    )
+    price_by_night = (
+        Column(Integer, nullable=False, default=0)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0
+    )
+    latitude = (
+        Column(Float, nullable=True)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0.0
+    )
+    longitude = (
+        Column(Float, nullable=True)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else 0.0
+    )
     amenity_ids = []
-    reviews = relationship(
-        "Review", cascade="all, delete, delete-orphan", backref="place"
+    reviews = (
+        relationship(
+            "Review", cascade="all, delete, delete-orphan", backref="place"
+        )
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else None
     )
-    amenities = relationship(
-        "Amenity",
-        secondary=place_amenity,
-        viewonly=False,
-        backref="place_amenities",
-    )
-
-    if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship(
+            "Amenity",
+            secondary=place_amenity,
+            viewonly=False,
+            backref="place_amenities",
+        )
+    else:
 
         @property
         def amenities(self):
